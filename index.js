e1f4fdadb8cd78f429f64ecc9cf376f52ed66045
hildamu <mukamihilda437@gmail.com>
2024-04-14 16:03:53 +0300

    submit

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+function submitData(name,email){
+
+  const userObject = {
+    name:name,
+    email:email
+  };
+   return fetch('http://localhost:3000/users',{
+    method:'POST',
+    headers:{
+      "Content-Type": "application/json",
+    "Accept": "application/json",
+    },
+    body:JSON.stringify(userObject)
+   })
+   .then(response => {
+    
+    if (!response.ok) {
+        throw new Error('Error');
+    }
+    return response.json();
+})
+.then(data => {
+    console.log('Data:', data);
+    
+    const newId = data.id;
+    
+    const idElement = document.createElement('p');
+    idElement.textContent = `New ID: ${newId}`;
+    document.body.appendChild(idElement);
+    return data;
+})
+.catch(error => {
+    
+    console.error('Error submission:', error);
+    
+
+    const errorElement = document.createElement('p');
+    errorElement.textContent = `Error:${error.message}`;
+    document.body.appendChild(errorElement);
+    
+});
+}
+  
